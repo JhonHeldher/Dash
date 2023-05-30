4d84479dbc8ce352653be3a0885fe75650ea1c13
Jhon Heldher <92549577+JhonHeldher@users.noreply.github.com>
2023-05-30 17:20:30 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from pandas._libs import properties
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
